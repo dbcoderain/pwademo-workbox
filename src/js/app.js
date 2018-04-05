7a438c898862c4ad4f82c1446d74cf77e912c448
Boyd <daera-boydd@nigov.net>
2018-04-05 15:50:14 +0100

    init

---
+++ src/js/app.js
@@ -0,0 +1,60 @@
+(function () {
+    // hack to view the error in browser
+    window.onerror = function (error) {
+        alert(error);
+    };
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/sw.js')
+                .then(registration => {
+                    console.log(`Service Worker registed! Scope: ${registration.scope}`);
+                })
+                .catch(err => {
+                    console.log(`Service Worker registration failed: ${err}`);
+                });
+        });
+    }
+
+})();
+
+
+
+function postData() {
+    debugger;
+
+    var message = {
+        name: 'David Test',
+        age: 40,
+        timeStamp: new Date().toLocaleDateString()
+    }
+
+
+    fetch('https://dbcoderainendpoint.herokuapp.com/notes', {
+        method: 'POST',
+        body: JSON.stringify(message),
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/json',
+        }
+    })
+        .then(response => response.text())
+        .then(text => {
+            console.log('response text is:' + text);
+            try {
+                return JSON.parse(text);
+                // Do your JSON handling here
+            } catch (err) {
+                // It is text, do you text handling here
+            }
+        }).then(function (data) {
+            if (data && data.success) {
+                console.log('Data Sent successfully...');
+            } else {
+                console.log('Response from service is not good...');
+            }
+            return;
+        });
+}
---
+++ src/sw.js
@@ -0,0 +1,124 @@
+/*
+Copyright 2018 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+
+    workbox.precaching.precacheAndRoute([]);
+
+    workbox.routing.registerRoute(
+        /(.*)articles(.*)\.(?:png|gif|jpg)/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'images-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                })
+            ]
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /(.*)images\/icon\/(.*)/,
+        workbox.strategies.staleWhileRevalidate({
+            cacheName: 'icon-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 5
+                })
+            ]
+        })
+    );
+
+    const articleHandler = workbox.strategies.networkFirst({
+        cacheName: 'articles-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 50
+            })
+        ]
+    });
+
+    workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+        return articleHandler.handle(args)
+            .then(response => {
+                if (!response) {
+                    return caches.match('pages/offline.html');
+                } else if (response.status === 404) {
+                    return caches.match('pages/404.html');
+                }
+                return response;
+            });
+    });
+
+
+
+    const postsHandler = workbox.strategies.cacheFirst({
+        cacheName: 'posts-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 50
+            })
+        ]
+    });
+
+    workbox.routing.registerRoute(/(.*)pages\/post(.*)\.html/, args => {
+        return postsHandler.handle(args)
+            .then(response => {
+                if (response && response.status === 404) {
+                    return caches.match('pages/404.html');
+                }
+                return response;
+            })
+            .catch(function () {
+                return caches.match('pages/offline.html');
+            });
+    });
+
+    // const bgSyncPlugin = new workbox.backgroundSync.Plugin('myQueueName', {
+    //     maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
+    //   });
+
+    //   workbox.routing.registerRoute(
+    //     /\/notes/,
+    //     workbox.strategies.networkOnly({
+    //       plugins: [bgSyncPlugin]
+    //     }),
+    //     'POST'
+    //   );
+
+    const queue = new workbox.backgroundSync.Queue('myQueueName2');
+
+    self.addEventListener('fetch', (event) => {
+        // Clone the request to ensure it's save to read when
+        // adding to the Queue.
+        const promiseChain = fetch(event.request.clone())
+            .then((response) => {
+                debugger;
+                return response;
+            })
+            .catch((err) => {
+                return queue.addRequest(event.request);
+            });
+
+        event.waitUntil(promiseChain);
+    });
+
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
